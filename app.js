d2987db4b61d06d85fd0a1e1e5d5bd291fd40210
keshavcodex1 <keshavcodex@gmail.com>
2021-10-02 19:16:00 +0530

    bcrypt hashing

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-
+const bcrypt = require('bcrypt');
+const saltRound = 10;
 const app = express();
 
 console.log(process.env.API_KEY);
@@ -39,18 +40,20 @@ app.get("/register", (req, res)=>{
 
 app.post("/login", (req, res)=>{
     const username = req.body.username;
-    const password = req.body.password
+    const password = req.body.password;
 
     User.findOne({email: username}, (err, foundUser)=>{
         if(err){
             console.log(err);
         } else {
-            if(foundUser){
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                } else {
-                    console.log("Wrong Password");
-                }
+            if(foundUser) {
+                bcrypt.compare(password, foundUser.password, (err, result)=>{
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        console.log("Wrong Password");
+                    }
+                });
             } else {
                 console.log("Wrong email");
             }
@@ -59,18 +62,21 @@ app.post("/login", (req, res)=>{
 });
 
 app.post("/register", (req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+    bcrypt.hash(req.body.password, saltRound, (err, hash)=>{
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save((err)=>{
+            if(!err){
+                res.render("secrets");
+            } else {
+                console.log("Registration Failed");
+            }
+        });
     });
 
-    newUser.save((err)=>{
-        if(!err){
-            res.render("secrets");
-        } else {
-            console.log("Registration Failed");
-        }
-    });
 });
 
 app.listen(process.env.PORT || 3000, (req, res)=>{
